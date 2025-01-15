7a81d581128c576b32ee5e915d6d09cfaeedc4aa
a-givertzman <a.givertzman@icloud.com>
2025-01-15 16:54:54 +0300

    Projects | FRDM

--- src/js/index.js
+++ src/js/index.js
@@ -31,14 +31,22 @@ document.addEventListener('DOMContentLoaded', function () {
   function adjustFontSize() {
     const parentWidth = document.querySelector(".first-screen-list").offsetWidth;
     let imageSizePercentage = 15.5;
+    let iconSizePercentage = 0.0;
     let scrollPosition = window.scrollY;
-    if (scrollPosition > 0) {
-      imageSizePercentage = 15.5;
+    if (scrollPosition > 250) {
+      imageSizePercentage = 0.0;
+      iconSizePercentage = 5.0;
     } else {
-      imageSizePercentage = 50;
+      imageSizePercentage = 50.0;
+      iconSizePercentage = 0.0;
     }
     let imageSize = (parentWidth * imageSizePercentage) / 100;
+    let iconSize = (parentWidth * iconSizePercentage) / 100;
     document.querySelector(".first-screen-menu-logo a img").style.width = imageSize + 'px';
+    // document.querySelector(".first-screen-menu-logo-icon").innerHTML = iconSize;
+    document.querySelector(".first-screen-menu-logo-icon").style.width = (iconSize + 1.0) + 'px';
+    document.querySelector(".first-screen-menu-logo-icon").style.maxWidth = (iconSize + 1.0) + 'px';
+    document.querySelector(".first-screen-menu-logo-icon a img").style.width = iconSize + 'px';
   }
   adjustFontSize();
   window.addEventListener('resize', function () {
